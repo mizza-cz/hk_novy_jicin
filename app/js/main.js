2564814694ed965e874ce99802384b5ea99bf7fe
mizza-cz <mizza.cz666@gmail.com>
2021-06-06 12:28:07 +0200

    add hp

---
+++ app/js/main.js
@@ -0,0 +1,52 @@
+$(function(){
+    // tabs team
+  $('.aside .aside__tab').on('click', function (event) {
+   var id = $(this).attr('data-id');
+   $('.aside').find('.aside__tab-content').removeClass('tab-active').hide();
+   $('.aside .aside__tabs').find('.aside__tab').removeClass('link');
+   $(this).addClass('link');
+   $('#' + id)
+     .addClass('tab-active')
+     .fadeIn();
+   return false;
+ });
+
+ $('.infoblock__wrapper .tabulka__tab').on('click', function (event) {
+   var id = $(this).attr('data-id');
+   $('.infoblock__wrapper').find('.infoblock__tabulka-content').removeClass('tab-active').hide();
+   $('.infoblock__wrapper .infoblock__header').find('.tabulka__tab').removeClass('active');
+   $(this).addClass('active');
+   $('#' + id)
+     .addClass('tab-active')
+     .fadeIn();
+   return false;
+ });
+
+ $('.infoblock__wrapper .statistiky__tab').on('click', function (event) {
+   var id = $(this).attr('data-id');
+   $('.infoblock__wrapper').find('.infoblock__statistiky-content').removeClass('tab-active').hide();
+   $('.infoblock__wrapper .infoblock__header').find('.statistiky__tab').removeClass('active');
+   $(this).addClass('active');
+   $('#' + id)
+     .addClass('tab-active')
+     .fadeIn();
+   return false;
+ });
+ $('.navbar__menu').on('click', function () {
+   $('.navigation__menu').slideToggle();
+ });
+ $('.navigation__item').on('click', function () {
+   $(this).closest('.navigation__list').toggleClass('active');
+ });
+});
+const menuBtn = document.querySelector('.navbar__menu');
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+  if (!menuOpen) {
+    menuBtn.classList.add('open');
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove('open');
+    menuOpen = false;
+  }
+});
